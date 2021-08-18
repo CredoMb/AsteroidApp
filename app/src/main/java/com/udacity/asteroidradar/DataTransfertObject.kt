9d4880ed2e164155524938fed48a5229e1fae35e
Credo-Mbikayi <credo.mbikayi@umontreal.ca>
2021-08-18 16:45:22 -0400

    First Commit
    This is the first commit

---
+++ app/src/main/java/com/udacity/asteroidradar/DataTransfertObject.kt
@@ -0,0 +1,22 @@
+package com.udacity.asteroidradar
+
+import com.squareup.moshi.JsonClass
+
+
+@JsonClass(generateAdapter = true)
+data class NetworkAsteroidContainer(val asteroids: List<NetworkAsteroid>)
+
+/**
+ * Represent a Asteroid from the API.
+ */
+@JsonClass(generateAdapter = true)
+data class NetworkAsteroid(
+    val id: String,
+    val absolute_magnitude_h: String,
+    val estimated_diameter_max: Int,
+    val is_potentially_hazardous_asteroid: Double,
+    val kilometers_per_second: String,
+    val astronomical: String)
+
+// Do we build all the small objects ?
+// I think so
---
+++ app/src/main/java/com/udacity/asteroidradar/api/NetworkUtils.kt
@@ -0,0 +1,104 @@
+package com.udacity.asteroidradar.api
+
+import com.jakewharton.retrofit2.adapter.kotlin.coroutines.CoroutineCallAdapterFactory
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import com.udacity.asteroidradar.Asteroid
+import com.udacity.asteroidradar.Constants
+import org.json.JSONObject
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import java.text.SimpleDateFormat
+import java.util.*
+import kotlin.collections.ArrayList
+
+// Get the result from the json
+// and return a list of asteroids
+fun parseAsteroidsJsonResult(jsonResult: JSONObject): ArrayList<Asteroid> {
+    val nearEarthObjectsJson = jsonResult.getJSONObject("near_earth_objects")
+
+    val asteroidList = ArrayList<Asteroid>()
+
+    val nextSevenDaysFormattedDates = getNextSevenDaysFormattedDates()
+    for (formattedDate in nextSevenDaysFormattedDates) {
+
+        // I should query the next 7 days
+        val dateAsteroidJsonArray = nearEarthObjectsJson.getJSONArray(formattedDate)
+
+        for (i in 0 until dateAsteroidJsonArray.length()) {
+            val asteroidJson = dateAsteroidJsonArray.getJSONObject(i)
+            val id = asteroidJson.getLong("id")
+            val codename = asteroidJson.getString("name")
+            val absoluteMagnitude = asteroidJson.getDouble("absolute_magnitude_h")
+            val estimatedDiameter = asteroidJson.getJSONObject("estimated_diameter")
+                .getJSONObject("kilometers").getDouble("estimated_diameter_max")
+
+            val closeApproachData = asteroidJson
+                .getJSONArray("close_approach_data").getJSONObject(0)
+            val relativeVelocity = closeApproachData.getJSONObject("relative_velocity")
+                .getDouble("kilometers_per_second")
+            val distanceFromEarth = closeApproachData.getJSONObject("miss_distance")
+                .getDouble("astronomical")
+            val isPotentiallyHazardous = asteroidJson
+                .getBoolean("is_potentially_hazardous_asteroid")
+
+            val asteroid = Asteroid(
+                id, codename, formattedDate, absoluteMagnitude,
+                estimatedDiameter, relativeVelocity, distanceFromEarth, isPotentiallyHazardous
+            )
+            asteroidList.add(asteroid)
+        }
+    }
+
+    return asteroidList
+}
+
+// return 7 next days ?
+private fun getNextSevenDaysFormattedDates(): ArrayList<String> {
+    val formattedDateList = ArrayList<String>()
+
+    val calendar = Calendar.getInstance()
+    for (i in 0..Constants.DEFAULT_END_DATE_DAYS) {
+        val currentTime = calendar.time
+        val dateFormat = SimpleDateFormat(Constants.API_QUERY_DATE_FORMAT, Locale.getDefault())
+
+        formattedDateList.add(dateFormat.format(currentTime))
+        calendar.add(Calendar.DAY_OF_YEAR, 1)
+    }
+
+    return formattedDateList
+}
+
+private fun getCurrentDateFormatted(): String {
+
+    val calendar = Calendar.getInstance()
+    val currentTime = calendar.time
+    val dateFormat = SimpleDateFormat(Constants.API_QUERY_DATE_FORMAT, Locale.getDefault())
+
+    return dateFormat.format(currentTime)
+}
+
+
+/**
+ * Build the Moshi object that Retrofit will be using, making sure to add the Kotlin adapter for
+ * full Kotlin compatibility.
+ */
+private val moshi = Moshi.Builder()
+    .add(KotlinJsonAdapterFactory())
+    .build()
+
+//neo/rest/v1/feed?start_date=...&api_key=XBPnbbUjscj2Mfn3gnEDQeSI5OFOsFdKdWP9Gcz6
+
+/**
+ * Main entry point for network access. Call like `Network.nearObjects.getPlaylist()`
+ */
+object Network {
+    // Configure retrofit to parse JSON and use coroutines
+    private val retrofit = Retrofit.Builder()
+        .baseUrl("https://api.nasa.gov/")
+        .addConverterFactory(MoshiConverterFactory.create(moshi))
+        .addCallAdapterFactory(CoroutineCallAdapterFactory())
+        .build()
+
+    //val nearObjects = retrofit.create(DevbyteService::class.java)
+}
