59cb70a307e5fa1401ed1eb3b030ab8b025425f7
Credo-Mbikayi <credo.mbikayi@umontreal.ca>
2021-08-25 15:15:05 -0400

    Add the code to fetch and store data in the Worker class, by overriding "doWork()"

--- app/src/main/java/com/udacity/asteroidradar/AsteroidRadarApplication.kt
+++ app/src/main/java/com/udacity/asteroidradar/AsteroidRadarApplication.kt
@@ -4,6 +4,8 @@ import android.app.Application
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import timber.log.Timber
+import java.util.concurrent.TimeUnit
 
 class AsteroidRadarApplication: Application() {
 
@@ -15,7 +17,20 @@ class AsteroidRadarApplication: Application() {
         }
     }
 
+    //
     private fun setupRecurringWork() {
         TODO("Not yet implemented")
+    }
+
+    /**
+     * onCreate is called before the first screen is shown to the user.
+     *
+     * Use it to setup any background tasks, running expensive setup operations in a background
+     * thread to avoid delaying app start.
+     */
+    override fun onCreate() {
+        super.onCreate()
+        Timber.plant(Timber.DebugTree())
+        delayedInit()
     }
 }
